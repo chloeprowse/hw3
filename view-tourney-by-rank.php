6c06e746cb789dbca8b955aa5c42b4a4b40e629a
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 13:23:55 -0600

    Create view-tourney-by-rank.php

---
+++ view-tourney-by-rank.php
@@ -0,0 +1,32 @@
+<h1>Rank by Tournament</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Number</th>
+        <th>Points</th>
+        <th>Tournamnet Name</th>
+        <th>Country</th>
+        <th>Day/Time</th> 
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+
+      while ($tourney = $tourneys->fetch_assoc()) { 
+      ?>
+        <tr>
+          <td><?php echo $tourney['rank_id']; ?></td>
+          <td><?php echo $tourney['rank_number']; ?></td>
+          <td><?php echo $tourney['total_points']; ?></td>
+          <td><?php echo $tourney['tourney_name']; ?></td>
+          <td><?php echo $tourney['country']; ?></td>
+          <td><?php echo $tourney['day_time']; ?></td>
+        </tr>
+      <?php
+      } 
+      ?>
+    </tbody>
+  </table>
+</div>
