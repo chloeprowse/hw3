0486c20c74194e07bb749b2a913574e39eaa5ffc
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 13:00:19 -0600

    Update model-tourney-by-wtennispro.php

--- model-tourney-by-wtennispro.php
+++ model-tourney-by-wtennispro.php
@@ -1,1 +1,16 @@
-
+<?php
+function selecttourneybywtennispro($wid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, tourney_name, country, day_time FROM `rank` r join tourney t on t.rank_id = r.rank_id where t.w_tennispro_id=?");
+        $stmt->bind_param("i", $wid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
