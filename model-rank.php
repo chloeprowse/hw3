e5a10b535fafb7c90d71518081af6d9623008702
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 08:00:36 -0600

    Update model-rank.php

--- model-rank.php
+++ model-rank.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectrank() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT rank_id, rank_number, total_points FROM `rank` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
