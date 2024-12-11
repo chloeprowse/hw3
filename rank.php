c0922cef1f4e42bbb76509e7c49345a0e407dd47
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 07:54:10 -0600

    Update rank.php

--- rank.php
+++ rank.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-rank.php");
 
+$pageTitle = "Rank";
+include "view-header.php";
+$rank = selectrank ();
+include "view-rank.php";
+include "view-footer.php";
+?>
