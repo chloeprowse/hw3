dc88dc17c075b20269ef2f80b3b3420ad1c7890d
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 07:11:18 -0600

    Update view-wtennispros.php

--- view-wtennispros.php
+++ view-wtennispros.php
@@ -3,21 +3,24 @@
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Country</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Country</th>
       </tr>
     </thead>
     <tbody>
-<?php
-while ($w_tennispro = $w_tennispro ->fetch_assoc()
-?>
-  <tr>
-    <td><?php echo $w_tennispro['w_tennispro_id']; ?></td>
-    <td><?php echo $w_tennispro['w_tennispro_name']; ?></td>
-    <td><?php echo $w_tennispro['country']; ?></td>
-  </tr>
-<?php 
-   </tbody>
+      <?php
+
+      while ($w_tennispro = $w_tennispro->fetch_assoc()) { // Fix missing closing parenthesis
+      ?>
+        <tr>
+          <td><?php echo $w_tennispro['w_tennispro_id']; ?></td>
+          <td><?php echo $w_tennispro['w_tennispro_name']; ?></td>
+          <td><?php echo $w_tennispro['country']; ?></td>
+        </tr>
+      <?php
+      } 
+      ?>
+    </tbody>
   </table>
 </div>
