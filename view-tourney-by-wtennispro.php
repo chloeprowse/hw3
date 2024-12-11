3213d4157d543e80e8889a2531ff5871bcc0b52b
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 13:06:54 -0600

    Create view-tourney-by-wtennispro.php

---
+++ view-tourney-by-wtennispro.php
@@ -0,0 +1,32 @@
+<h1>Women's Tennis Tournaments</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Number</th>
+        <th>Points</th>
+        <th>Tournamnet Name</th>
+        <th>Country</th>
+        <th>Day/Time</th> 
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+
+      while ($ranks = $rank->fetch_assoc()) { 
+      ?>
+        <tr>
+          <td><?php echo $ranks['rank_id']; ?></td>
+          <td><?php echo $ranks['rank_number']; ?></td>
+          <td><?php echo $ranks['total_points']; ?></td>
+          <td><?php echo $ranks['tourney_name']; ?></td>
+          <td><?php echo $ranks['country']; ?></td>
+          <td><?php echo $ranks['day_time']; ?></td>
+        </tr>
+      <?php
+      } 
+      ?>
+    </tbody>
+  </table>
+</div>
