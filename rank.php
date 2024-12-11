10b7f9f1ac1d9ff6135c6f73fa3b724caa6a2b4a
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 17:10:30 -0600

    Update rank.php

--- rank.php
+++ rank.php
@@ -4,6 +4,14 @@ require_once("model-rank.php");
 
 $pageTitle = "Rank";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+     insertrank($_POST['rnumber'], $_POST['rtotalpoints']);
+     break;
+  }
+}
 $rank = selectrank ();
 include "view-rank.php";
 include "view-footer.php";
