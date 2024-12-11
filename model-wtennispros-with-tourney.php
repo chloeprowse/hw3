2319c70a9e9e648f157701a66908f89b4ebf4740
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 14:05:29 -0600

    Update model-wtennispros-with-tourney.php

--- model-wtennispros-with-tourney.php
+++ model-wtennispros-with-tourney.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectwomenstennispros() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT w_tennispro_id, w_tennispro_name, country FROM `w_tennispro` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
