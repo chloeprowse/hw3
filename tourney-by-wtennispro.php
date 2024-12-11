a2115d53c8d8d5c0d75c6932c9db62d8533209ff
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 12:20:05 -0600

    Update tourney-by-wtennispro.php

--- tourney-by-wtennispro.php
+++ tourney-by-wtennispro.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-tourney-by-wtennispro.php");
 
+$pageTitle = "Womens Tennis Tournamets";
+include "view-header.php";
+$rank = selecttourneybywtennispro ($_GET['id']);
+include "view-tourney-by-wtennispro.php";
+include "view-footer.php";
+?>
