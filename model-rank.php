6c0c0c576da8ca37827d3f197d611d73dd6e09ab
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 15:37:19 -0600

    Update model-rank.php

--- model-rank.php
+++ model-rank.php
@@ -12,4 +12,43 @@ function selectrank() {
         throw $e;
     }
 }
+function insertrank($rnumber,$rtotalpoints) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `rank` (`rank_number`, `total_points`) VALUES ('?', '?'); ");
+        $stmt->bind_param("ss", $rnumber, $rtotalpoints);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updaterank($rnumber,$rtotalpoints, $rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `rank` set `rank_number` = ?, `total_points` = ? where rank_id = ?");
+        $stmt->bind_param("ssi", $rnumber, $rtotalpoints, $rid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleterank($rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from rank where rank_id=?");
+        $stmt->bind_param("i", $rid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
