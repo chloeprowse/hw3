f191d15045d6ccf6fbda3a1f171290285c88aafb
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 13:33:51 -0600

    Update view-rank.php

--- view-rank.php
+++ view-rank.php
@@ -6,6 +6,7 @@
         <th>ID</th>
         <th>Number</th>
         <th>Points</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -14,9 +15,15 @@
       while ($ranks = $rank->fetch_assoc()) { 
       ?>
         <tr>
-          <td><?php echo $ranks['rank_id']; ?></td>
+          <td><?php echo $ranks['rank_id']; ?></td>                   
           <td><?php echo $ranks['rank_number']; ?></td>
           <td><?php echo $ranks['total_points']; ?></td>
+          <td> 
+          <form method="post" action="tourney-by-rank.php">
+              <input type="hidden" name="rid" value="<?php echo $ranks['rank_id']; ?>">
+              <button type="submit" class="btn btn-primary">Tournaments</button>
+            </form>
+          </td>
         </tr>
       <?php
       } 
