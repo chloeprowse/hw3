b8b6d65b60c4af5808d93ea910c7d2f9bf1e1bfb
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 14:36:30 -0600

    Update view-wtennispros-with-tourney.php

--- view-wtennispros-with-tourney.php
+++ view-wtennispros-with-tourney.php
@@ -21,14 +21,17 @@
     <div class="card-body">
       <h5 class="card-title"><?php echo $womenstennispro['w_tennispro_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
         <?php 
         $tourneys = selectwtennisprosbytourney($womenstennispro['w_tennispro_id']);
         while ($tourney = $tourneys->fetch_assoc()) {
         ?>
+          <li class="list-group-item"><?php echo $ranks['rank_number']; ?> - <?php echo $ranks['total_points']; ?> - <?php echo $ranks['tourney_name']; ?> - <?php echo $ranks['country']; ?> - <?php echo $ranks['day_time']; ?></li>
         
         <?php
         }
         ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Country Origin: <?php echo $womenstennispro['country']; ?></small></p>
     </div>
