432cf0f05bddbd498646855c283499922660077e
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 13:17:25 -0600

    Create tourney-by-rank.php

---
+++ tourney-by-rank.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-tourney-by-rank.php");
+
+$pageTitle = "Tournament by Rank";
+include "view-header.php";
+$tourney = selecttourneybyrank ($_POST['rid']);
+include "view-tourney-by-rank.php";
+include "view-footer.php";
+?>
