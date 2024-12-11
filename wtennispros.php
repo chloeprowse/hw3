f7caeea66cd93fa03fbd37106e45ec9330f170c8
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 06:59:41 -0600

    Update and rename womenstennispros.php to wtennispros.php

--- wtennispros.php
+++ wtennispros.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-womenstennispros.php");
+require_once("model-wtennispros.php");
 
 $pageTitle = "Womens Tennis Pros";
 include "view-header.php";
 $womenstennispros = selectwomenstennispros ();
-include "view-womenstennispros.php";
+include "view-wtennispros.php";
 include "view-footer.php";
 ?>
