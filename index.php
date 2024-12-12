20de34b8940c9a32fd4b57f5e08f38b54f23cefb
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 13:56:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitle = '<img src="https://th.bing.com/th/id/R.21eafaf0b870d9fd299ebd2da8a03977?rik=wOzB8D%2fEVam5Zg&pid=ImgRaw&r=0" alt="A tennis ball" style="width: 50px; height: auto; border: none; border-radius: 10px;">';
+$pageTitle = "Home";
 include "view-header.php";
 ?>
 
