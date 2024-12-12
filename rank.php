59bbd48a9fbca624c4fa3f03f0cdfa5fe90d3a73
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 18:44:49 -0600

    Update rank.php

--- rank.php
+++ rank.php
@@ -6,24 +6,24 @@ $pageTitle = "Rank";
 include "view-header.php";
 
 if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
+  switch ($_POST['actionType'])) {
     case "Add":
-     if(insertrank($_POST['rnumber'], $_POST['rtotalpoints'])) {
-       echo '<div class="alert alert-success" role="alert">Rank added.</div>';
-     } else {
-       echo '<div class="alert alert-danger" role="alert">Error.</div>';
-     }
-     break;
-     case "Delete":
-     if(deleterank($_POST['rid']])) {
-       echo '<div class="alert alert-success" role="alert">Rank deleted.</div>';
-     } else {
-       echo '<div class="alert alert-danger" role="alert">Error.</div>';
-     }
-     break;
+      if (insertrank($_POST['rnumber'], $_POST['rtotalpoints'])) {
+        echo '<div class="alert alert-success" role="alert">Rank added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleterank($_POST['rid'])) {
+        echo '<div class="alert alert-success" role="alert">Rank deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
   }
 }
-$rank = selectrank ();
+$rank = selectrank();
 include "view-rank.php";
 include "view-footer.php";
 ?>
