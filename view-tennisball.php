ea609d8039859d7e199c89caa935b59a5e285341
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 20:24:03 -0600

    Update view-tennisball.php

--- view-tennisball.php
+++ view-tennisball.php
@@ -1,4 +1,13 @@
+<div class="row"
+  <div class="col">
 <h1>Tennis Balls Used</h1>
+  </div>
+  <div class="col-auto">
+    <?php
+include "view-tennisball-newform.php"
+?>
+</div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -6,7 +15,10 @@
         <th>ID</th>
         <th>Brand</th>
         <th>Color</th>
-        <th>Womens Pro ID</th>   
+        <th>Womens Pro ID</th>  
+          <th></th>
+          <th></th>
+          <th></th>
       </tr>
     </thead>
     <tbody>
@@ -19,6 +31,23 @@
           <td><?php echo $tennisballs['tb_brand']; ?></td>
           <td><?php echo $tennisballs['tb_color']; ?></td>
           <td><?php echo $tennisballs['w_tennispro_id']; ?></td>
+           <td> 
+          <?php
+          include "view-tennisball-editform.php"
+          ?>
+          </td>
+          <td>
+            <form method="post" action="">
+              <input type="hidden" name="rid" value="<?php echo $tennisballs['tennisball_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+              <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                </svg>
+              </button>
+            </form>
+          </td>
         </tr>
       <?php
       } 
@@ -26,3 +55,4 @@
     </tbody>
   </table>
 </div>
+
