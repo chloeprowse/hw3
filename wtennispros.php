ce6b78292001eb713cf9c2520e2f8d83ae76a743
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 16:00:40 -0600

    Update wtennispros.php

--- wtennispros.php
+++ wtennispros.php
@@ -3,18 +3,18 @@ require_once("util-db.php");
 require_once("model-wtennispros.php");
 
 $pageTitle = "Womens Tennis Pros";
-include "view-header.php";
+include "view-header.php";          
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) { 
     case "Add":
-      if (insertwomenstennispros($_POST['name'], $_POST['country'])) {
+      if (insertwomenstennispros($_POST['name'], $_POST['country'], $_POST['ranknum'], $_POST['totalpoints'], $_POST['tourneyname'],  $_POST['tcountry'],  $_POST['daytime'])) {
         echo '<div class="alert alert-success" role="alert">Womens Tennis Pro added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updatewomenstennispros($_POST['name'], $_POST['country'], $_POST['wid'])) {
+      if (updatewomenstennispros($_POST['name'], $_POST['country'], $_POST['ranknum'], $_POST['totalpoints'], $_POST['tourneyname'],  $_POST['tcountry'],  $_POST['daytime'] $_POST['wid'])) {
         echo '<div class="alert alert-success" role="alert">Womens Tennis Pro edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
