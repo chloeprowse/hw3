ec814fbf94b826b4cc5a4a0822e428fbfd20fdd0
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 10:31:57 -0600

    Update wtennispros-with-tourney.php

--- wtennispros-with-tourney.php
+++ wtennispros-with-tourney.php
@@ -7,21 +7,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) { 
     case "Add":
-      if (insertwomenstennispros($_POST['name'], $_POST['country'])) {
+      if (inserttourneybywtennispro($_POST['name'], $_POST['country'])) {
         echo '<div class="alert alert-success" role="alert">Womens Tennis Pro added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updatewomenstennispros($_POST['name'], $_POST['country'], $_POST['wid'])) {
+      if (updatetourneybywtennispro($_POST['name'], $_POST['country'], $_POST['wid'])) {
         echo '<div class="alert alert-success" role="alert">Womens Tennis Pro edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-      if (deletewomenstennispros($_POST['wid'])) {
+      if (deletetourneybywtennispro($_POST['wid'])) {
         echo '<div class="alert alert-success" role="alert">Womens Tennis Pro deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
