31a44ffc48a62fef7aff56a6117aab7df821ed36
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 10:19:39 -0600

    Update model-wtennispros-with-tourney.php

--- model-wtennispros-with-tourney.php
+++ model-wtennispros-with-tourney.php
@@ -28,5 +28,57 @@ function selecttourneybywtennispro($wid) {
         throw $e;
     }
 }
+function inserttourneybywtennispro($tourneyName, $country, $rank, $totalpoints, $day, $rid, $wid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `tourney` (`tourney_name`, `country`, `rank_number`, `total_points`, `day_time`, `r.rank_id`,  `w_tennispro_id`) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssii", $tourneyName, $country, $rank, $totalpoints, $day, $rid, $wid,);
+        $stmt->execute();
+        $success = $stmt->affected_rows > 0;
+        $stmt->close();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        if (isset($conn)) $conn->close();
+        throw $e;
+    }
+}
+
+function updatetourneybywtennispro($tourneyName, $country, $rank, $totalpoints, $day, $tourneyId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `tourney` 
+                                SET `tourney_name` = ?, `country` = ?, `rank_number` = ?, `total_points` = ?, `day_time` = ?
+                                WHERE tourney_id = ?");
+        $stmt->bind_param("sssssi", $tourneyName, $country, $rank, $totalpoints, $day, $tourneyId);
+        $stmt->execute();
+        $success = $stmt->affected_rows > 0;
+        $stmt->close();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        if (isset($conn)) $conn->close();
+        throw $e;
+    }
+}
+
+function deletetourneybywtennispro($tourneyId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `tourney` 
+                                WHERE tourney_id = ?");
+        $stmt->bind_param("i", $tourneyId);
+        $stmt->execute();
+        $success = $stmt->affected_rows > 0;
+        $stmt->close();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        if (isset($conn)) $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
 
