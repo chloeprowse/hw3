2e16a0eca37b77d7e7b137b4c233587d97f86c59
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 20:39:23 -0600

    Update tennisball.php

--- tennisball.php
+++ tennisball.php
@@ -4,6 +4,31 @@ require_once("model-tennisball.php");
 
 $pageTitle = "Tennis Balls Used";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) { 
+    case "Add":
+      if (inserttennisball($_POST['tbbrand'], $_POST['tbcolor'], $_POST['wid'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updatetennisball($_POST['tbbrand'], $_POST['tbcolor'], $_POST['wid'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deletetennisball($_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
 $tennisball = selecttennisball ();
 include "view-tennisball.php";
 include "view-footer.php";
