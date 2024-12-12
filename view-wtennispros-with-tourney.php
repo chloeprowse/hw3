c227e0b401300708dd654992d563f27f4f6f07f6
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 09:57:46 -0600

    Update view-wtennispros-with-tourney.php

--- view-wtennispros-with-tourney.php
+++ view-wtennispros-with-tourney.php
@@ -1,51 +1,59 @@
-<div class="row"
+<div class="row">
   <div class="col">
-<h1>Womens Tennis Pros Tournamnets</h1>
+    <h1>Womens Tennis Pros Tournaments</h1>
   </div>
   <div class="col-auto">
-    <?php
-include "view-wtennispros-with-tourney-newform.php"
-?>
+    <?php include "view-wtennispros-with-tourney-newform.php"; ?>
+  </div>
 </div>
-</div>
+
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
+        <!-- Add headers if needed -->
       </tr>
     </thead>
     <tbody>
-      <?php
+      <?php while ($womenstennispro = $womenstennispros->fetch_assoc()) { ?>
+        <div class="card-group">
+          <div class="card">
+            <?php include "view-wtennispros-with-tourney-editform.php"; ?>
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $womenstennispro['w_tennispro_name']; ?></h5>
+              <p class="card-text">
+                <ul class="list-group">
+                  <?php 
+                  $tourneys = selecttourneybywtennispro($womenstennispro['w_tennispro_id']);
+                  while ($tourney = $tourneys->fetch_assoc()) { ?>
+                    <li class="list-group-item">
+                      <?php echo $tourney['rank_number'] . ' - ' . 
+                                 $tourney['total_points'] . ' - ' . 
+                                 $tourney['tourney_name'] . ' - ' . 
+                                 $tourney['country'] . ' - ' . 
+                                 $tourney['day_time']; ?>
+                      
+                      <form method="post" action="">
+                        <input type="hidden" name="wid" value="<?php echo $womenstennispro['w_tennispro_id']; ?>">
+                        <input type="hidden" name="actionType" value="Delete">
+                        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                        </button>
+                      </form>
+                    </li>
+                  <?php } ?>
+                </ul>
+              </p>
+              <p class="card-text"><small class="text-body-secondary">Country Origin: <?php echo $womenstennispro['country']; ?></small></p>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+    </tbody>
+  </table>
+</div>
 
-      while ($womenstennispro = $womenstennispros->fetch_assoc()) { 
-      ?>
-       <div class="card-group">
-  <div class="card">
-     <?php
-          include "view-wtennispros-with-tourney-editform.php"
-          ?>
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $womenstennispro['w_tennispro_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-        <?php 
-        $tourneys = selecttourneybywtennispro($womenstennispro['w_tennispro_id']);
-        while ($tourney = $tourneys->fetch_assoc()) {
-        ?>
-          <li class="list-group-item"><?php echo $tourney['rank_number']; ?> - <?php echo $tourney['total_points']; ?> - <?php echo $tourney['tourney_name']; ?> - <?php echo $tourney['country']; ?> - <?php echo $tourney['day_time']; ?></li>
-        
-        <?php
-          
-        }
-        ?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Country Origin: <?php echo $womenstennispro['country']; ?></small></p>
-    </div>
-  </div>
-       
-  <?php
-  } 
-  ?>
-  </div>
     
