7f2f75ead0ed93b101960f706b5c14e3e9765764
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 11:23:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 <h1>Who doesn't love tennis?</h1>
+
+<!-- Add a photo -->
+<div class="container text-center mt-4">
+    <img src="https://www.usmagazine.com/wp-content/uploads/2019/07/Camila-Giorgi-Wimbledon-Tennis-Outfits.jpg?quality=86&strip=all" alt="Tennis" class="img-fluid" style="max-width: 100%; height: auto; border-radius: 10px;">
+    <p class="mt-2">Tennis is a game loved by many around the world!</p>
+</div>
+
 <?php
 include "view-footer.php";
-?>  
+?>
