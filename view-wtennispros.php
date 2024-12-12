3f69ce434b30e97353c29f4e6108f07a9c9502c0
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 13:40:11 -0600

    Update view-wtennispros.php

--- view-wtennispros.php
+++ view-wtennispros.php
@@ -1,6 +1,6 @@
 <div class="row"
   <div class="col">
-<h1>Womens Tennis Pros <img src="https://www.creativefabrica.com/wp-content/uploads/2022/01/20/Tennis-Player-Vector-Young-And-Healthy-Graphics-23800328-1.jpg" alt="A tennis ball" style="width: 100px; height: auto; border: 0px solid black; border-radius: 10px;"></h1>
+<h1>Womens Tennis Pros <img src="https://www.creativefabrica.com/wp-content/uploads/2022/01/20/Tennis-Player-Vector-Young-And-Healthy-Graphics-23800328-1.jpg" alt="A tennis ball" style="width: 200px; height: auto; border: 0px solid black; border-radius: 10px;"></h1>
   </div>
   <div class="col-auto">
     <?php
