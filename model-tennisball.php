1e8f0a251fbdc254b0b64d1e7613c2cb1569194f
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 20:58:47 -0600

    Update model-tennisball.php

--- model-tennisball.php
+++ model-tennisball.php
@@ -16,7 +16,7 @@ function inserttennisball($tbbrand, $tbcolor, $wid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `tennisball` (`tb_brand`, `tb_color`,`w_tennispro_id`) VALUES (?, ?, ?);");
-        $stmt->bind_param("ssi", $tbbrand, $tbcolor, $wid);
+        $stmt->bind_param("sss", $tbbrand, $tbcolor, $wid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -29,7 +29,7 @@ function updatetennisball($tbbrand, $tbcolor, $wid, $tid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `tennisball` set `tb_brand` = ?, `tb_color` = ?, `w_tennispro_id` = ? where tennisball_id = ?");
-        $stmt->bind_param("sss", $tbbrand, $tbcolor, $wid, $tid);
+        $stmt->bind_param("sssi", $tbbrand, $tbcolor, $wid, $tid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
