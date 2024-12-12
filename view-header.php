3919174bb8e9c3636f73aa7390b3d855059aafad
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 13:55:40 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,9 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <header>
-        <h1><?php echo $pageTitle; ?></h1> 
-    </header>
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
@@ -20,7 +17,8 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="/"><header>
+        <h1><?php echo $pageTitle; ?></h1> </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="wtennispros.php">Womens Tennis Pros</a>
