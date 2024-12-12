d3df2eab0b2f0270f10e94f5c394be3d458906b1
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 21:28:00 -0600

    Update model-wtennispros.php

--- model-wtennispros.php
+++ model-wtennispros.php
@@ -10,6 +10,44 @@ function selectwomenstennispros() {
     } catch (Exception $e) {
         $conn->close();
         throw $e;
+        
+    function inserttennisball($tbbrand, $tbcolor, $wid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `tennisball` (`tb_brand`, `tb_color`,`w_tennispro_id`) VALUES (?, ?, ?);");
+        $stmt->bind_param("sss", $tbbrand, $tbcolor, $wid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updatetennisball($tbbrand, $tbcolor, $tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `tennisball` set `tb_brand` = ?, `tb_color` = ? where tennisball_id = ?");
+        $stmt->bind_param("ssi", $tbbrand, $tbcolor, $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deletetennisball($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `tennisball` where tennisball_id=?");
+        $stmt->bind_param("i", $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
     }
 }
 ?>
