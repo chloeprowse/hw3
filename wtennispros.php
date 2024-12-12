a6a1a9fca4e313e1ef8f4e9d82646b39c186861f
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-11 21:25:55 -0600

    Update wtennispros.php

--- wtennispros.php
+++ wtennispros.php
@@ -4,6 +4,31 @@ require_once("model-wtennispros.php");
 
 $pageTitle = "Womens Tennis Pros";
 include "view-header.php";
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) { 
+    case "Add":
+      if (insert($_POST[''], $_POST['tbcolor'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updatetennisball($_POST['tbbrand'], $_POST['tbcolor'], $_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deletetennisball($_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Tennis Ball deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  } 
+}
 $womenstennispros = selectwomenstennispros ();
 include "view-wtennispros.php";
 include "view-footer.php";
