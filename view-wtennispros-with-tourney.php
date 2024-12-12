3375a4a8929b5af5cf647e7a183c46619145fa76
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 12:53:09 -0600

    Update view-wtennispros-with-tourney.php

--- view-wtennispros-with-tourney.php
+++ view-wtennispros-with-tourney.php
@@ -1,7 +1,13 @@
 <div class="row"
   <div class="col">
 <h1>Womens Tennis Pros Tournaments</h1>
-
+ </div>
+  <div class="col-auto">
+    <?php
+include "view-wtennispros-newform.php"
+?>
+</div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
