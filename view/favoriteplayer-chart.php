42c08f62ad01d6333dbe90666ab6acbca10b484c
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 22:55:29 -0600

    Update favoriteplayer-chart.php

--- view/favoriteplayer-chart.php
+++ view/favoriteplayer-chart.php
@@ -1,8 +1,36 @@
-
 <h1>Favorite Player Chart></h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data:  {
+    datasets: [{
+        data: 
 <?php
       while ($favoriteplayers = $favoriteplayer->fetch_assoc()) { 
+         echo $favoriteplayers['num_favoriteplayer'] . ", ";
+      }
       ?>
-        
+            
+    }],
+
+    labels: [
+        <?php
+      $favorite
+      while ($favoriteplayers = $favoriteplayer->fetch_assoc()) { 
+         echo $favoriteplayers['num_favoriteplayer'] . ", ";
+      }
+      ?>
+    ]
+},
+    
+  });
+</script>        
     
        
