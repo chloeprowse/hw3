4ee953de607ae3002d31530e9dd0814f7d24affb
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 19:42:25 -0600

    Update view-tennisball-newform.php

--- view-tennisball-newform.php
+++ view-tennisball-newform.php
@@ -21,7 +21,6 @@
     <label for="tbbrand" class="form-label">Tennis Ball Brand</label>
     <?php
 $tbbrandList = selecttennisballForInput();
-$selectedtbbrand = 0;
 include "view-tbbrand-input-list.php";
 ?>
   </div>
