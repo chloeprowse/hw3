8e4c073ac696546de868da960b15be8db2bad4f3
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 22:27:08 -0600

    Update favoriteplayer-chart.php

--- model/favoriteplayer-chart.php
+++ model/favoriteplayer-chart.php
@@ -1,1 +1,14 @@
-
+<?php
+function selectfavoriteplayer() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT name, count(favoriteplayer) as num_favoriteplayer FROM `favoriteplayer` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
