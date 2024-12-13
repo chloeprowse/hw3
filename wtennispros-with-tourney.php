fcfea6c4f15f94068fda0249d34e08afad4f63c3
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 21:44:33 -0600

    Update wtennispros-with-tourney.php

--- wtennispros-with-tourney.php
+++ wtennispros-with-tourney.php
@@ -1,6 +1,6 @@
 <?php
 require_once("util-db.php");
-require_once("model-wtennispros-with-tourney.php");
+require_once("model/wtennispros-with-tourney.php");
 
 $pageTitle = "Womens Tennis Pros Tournaments";
 include "view-header.php";
