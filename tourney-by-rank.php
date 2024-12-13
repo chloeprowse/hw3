127d89c2927c8bd600a6d07d200075b73f2ca7e4
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 21:53:36 -0600

    Update tourney-by-rank.php

--- tourney-by-rank.php
+++ tourney-by-rank.php
@@ -5,6 +5,6 @@ require_once("model/tourney-by-rank.php");
 $pageTitle = "Tournament by Rank";
 include "view-header.php";
 $tourney = selecttourneybyrank ($_POST['rid']);
-include "view-tourney-by-rank.php";
-include "view-footer.php";
+include "view/tourney-by-rank.php";
+include "view/footer.php";
 ?>
