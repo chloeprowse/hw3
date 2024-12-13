8bd7b7fc327a27675ee360fc71488fbbf8d06a0a
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 22:22:07 -0600

    Update favoriteplayer-chart.php

--- favoriteplayer-chart.php
+++ favoriteplayer-chart.php
@@ -1,36 +1,12 @@
 <?php
 require_once("util-db.php");
 require_once("model/favoriteplayer.php");
+require_once("model/favoriteplayer-chart.php");
 
-$pageTitle = "Add your favorite tennis player!";
+$pageTitle = "Favorite Player Chart";
 include "view/header.php";
 
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) { 
-    case "Add":
-      if (insertfavoriteplayer($_POST['name'], $_POST['player'])) {
-        echo '<div class="alert alert-success" role="alert">Favorite Player added.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Edit":
-      if (updatefavoriteplayer($_POST['name'], $_POST['player'], $_POST['fid'])) {
-        echo '<div class="alert alert-success" role="alert">Favorite Player edited.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Delete":
-      if (deletefavoriteplayer($_POST['fid'])) {
-        echo '<div class="alert alert-success" role="alert">Favorite Player deleted.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-  }
-}
 $favoriteplayer = selectfavoriteplayer();
-include "view/favoriteplayer.php";
+include "view/favoriteplayer-chart.php";
 include "view/footer.php";
 ?>
