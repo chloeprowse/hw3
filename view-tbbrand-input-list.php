93b340abee0225efc765c1169ff431647885e07b
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 19:35:59 -0600

    Update view-tbbrand-input-list.php

--- view-tbbrand-input-list.php
+++ view-tbbrand-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="tbbrand" name="tbbrand">
   <?php
 while ($tbbrandItem = $tbbrandList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedtbbrand == $tbbrandItem['tb_brand'] {
+$selText = "selected";
+      }
 ?>
-  <option value="<?php echo $tbbrandItem['tb_brand'];?>"><?php echo $tbbrandItem['tb_brand'];?></option>
+  <option value="<?php echo $tbbrandItem['tb_brand'];?>"<?-selText?>><?php echo $tbbrandItem['tb_brand'];?></option>
   <?php
 }
 ?>
