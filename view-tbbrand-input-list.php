04eacbde2b790a0cabb45df92c12f38ee207ddd7
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 19:37:39 -0600

    Update view-tbbrand-input-list.php

--- view-tbbrand-input-list.php
+++ view-tbbrand-input-list.php
@@ -1,12 +1,8 @@
 <select class="form-select" id="tbbrand" name="tbbrand">
   <?php
 while ($tbbrandItem = $tbbrandList->fetch_assoc()) {
-  $selText = "";
-  if ($selectedtbbrand == $tbbrandItem['tb_brand'] {
-$selText = "selected";
-      }
-?>
-  <option value="<?php echo $tbbrandItem['tb_brand'];?>"<?-selText?>><?php echo $tbbrandItem['tb_brand'];?></option>
+  
+  <option value="<?php echo $tbbrandItem['tb_brand'];?>"selected<?php echo $tbbrandItem['tb_brand'];?></option>
   <?php
 }
 ?>
