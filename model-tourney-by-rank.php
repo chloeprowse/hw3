1b64ecb700aca601aaec17dc2990d66d83e3608f
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 18:24:17 -0600

    Update model-tourney-by-rank.php

--- model-tourney-by-rank.php
+++ model-tourney-by-rank.php
@@ -2,7 +2,7 @@
 function selecttourneybyrank($rid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, tourney_name, country, day_time FROM `rank` r join tourney t on t.rank_id = r.rank_id where t.rank_id=?");
+        $stmt = $conn->prepare("SELECT r.rank_id, rank_number, total_points, tourney_name, tcountry, day_time FROM `rank` r join tourney t on t.rank_id = r.rank_id where t.rank_id=?");
         $stmt->bind_param("i", $rid);
         $stmt->execute();
         $result = $stmt->get_result();
