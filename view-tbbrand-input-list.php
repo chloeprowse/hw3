32e1f5e51660d306ea885a10b31c3f55b429f79b
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-12 19:19:20 -0600

    Update view-tbbrand-input-list.php

--- view-tbbrand-input-list.php
+++ view-tbbrand-input-list.php
@@ -1,8 +1,8 @@
-<select class="form-select" id="tid" name="tid">
+<select class="form-select" id="tbbrand" name="tbbrand">
   <?php
 while ($tbbrandItem = $tbbrandList->fetch_assoc()) {
 ?>
-  <option value="<?php echo $tbbrandItem['tennisball_id'];?>"><?php echo $tbbrandItem['tb_brand'];?></option>
+  <option value="<?php echo $tbbrandItem['tb_brand'];?>"><?php echo $tbbrandItem['tb_brand'];?></option>
   <?php
 }
 ?>
